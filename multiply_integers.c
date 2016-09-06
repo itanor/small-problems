cd930d14f864e20dc75f1772f2a3c35146a4e72d
Itanor Strapazzon <itanor@gmail.com>
2016-09-06 00:53:09 -0300

    first version

--- multiply_integers.c
+++ multiply_integers.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("hello\n");
+int multiply_integers(int n1, int n2) {
+  int times = abs(n1);
+  int result = 0; 
+  for(int i = 0; i < times; i++) {
+    result += n2;
+  }
+  return result;
+}
+
+int main(int argc, char *argv[]) {
+  printf("%i\n", multiply_integers(-2, 3));
   return 0;
 }
 
